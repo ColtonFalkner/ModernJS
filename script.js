f112e8f2315983dcf0a36d2709f6c6b495d4d8a8
Colton <coltonfalkner@gmail.com>
2021-05-25 18:02:09 -0400

    played with parcel as much as im willing to for today

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 'use strict'
+if (module.hot) {
+  module.hot.accept()
+}
 // //Importing
 // import { addToCart, totalPrice as price, tq } from './shoppingCart.js'
 // console.log('Importing module')
@@ -11,35 +14,52 @@
 // ShoppingCart.addToCart('pepperonis', 56)
 // console.log(ShoppingCart.totalPrice)
 
-// import add, { cart } from './shoppingCart.js'
-// add('breads', 6)
-// add('pizza', 2)
-// add('apple', 4)
+import add, { cart } from './shoppingCart.js'
+add('breads', 6)
+add('pizza', 2)
+add('apple', 4)
 
 // console.log(cart)
 
-const shoppingCart2 = (function () {
-  const cart = []
-  const shippingCost = 10
-  const totalPrice = 237
-  const totalQuantity = 23
+// const shoppingCart2 = (function () {
+//   const cart = []
+//   const shippingCost = 10
+//   const totalPrice = 237
+//   const totalQuantity = 23
 
-  const addToCart = function (product, quantity) {
-    cart.push({ product, quantity })
-    console.log(`${quantity} ${product} added to cart.`)
-  }
+//   const addToCart = function (product, quantity) {
+//     cart.push({ product, quantity })
+//     console.log(`${quantity} ${product} added to cart.`)
+//   }
 
-  const orderStock = function (product, quantity) {
-    console.log(`${quantity} ${product} ordered from supplier.`)
-  }
+//   const orderStock = function (product, quantity) {
+//     console.log(`${quantity} ${product} ordered from supplier.`)
+//   }
 
-  return {
-    addToCart,
-    cart,
-    totalPrice,
-    totalQuantity,
-  }
-})()
+//   return {
+//     addToCart,
+//     cart,
+//     totalPrice,
+//     totalQuantity,
+//   }
+// })()
 
-shoppingCart2.addToCart('apple', 4)
-shoppingCart2.addToCart('pizza', 2)
+// shoppingCart2.addToCart('apple', 4)
+// shoppingCart2.addToCart('pizza', 2)
+
+import cloneDeep from 'lodash'
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+}
+
+const stateClone = Object.assign({}, state)
+const stateDeepClone = cloneDeep(state)
+console.log(stateClone)
+state.user.loggedIn = false
+console.log(stateDeepClone)
+console.log(state)
